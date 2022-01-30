5fde608aca6759c3e523d3264cda6ee7147425d3
ron <rinbar70@gmail.com>
2022-01-30 21:04:55 +0200

    Format with Prettier.

--- src/curry.ts
+++ src/curry.ts
@@ -4,11 +4,14 @@ function curry<T>(n: number, f: (...args: any[]) => T): (arg0: any) => any {
         : f;
 }
 
-function curry2<S0, S1, T>(f: (arg0: S0, arg1: S1) => T): (arg0: S0) => (arg1: S1) => T {
+function curry2<S0, S1, T>(
+    f: (arg0: S0, arg1: S1) => T
+): (arg0: S0) => (arg1: S1) => T {
     return (arg0: S0) => (arg1: S1) => f(arg0, arg1);
 }
 
-function curry3<S0, S1, S2, T>(f: (arg0: S0, arg1: S1, arg2: S2) => T): (arg0: S0) => (arg1: S1) => (arg2: S2) => T {
+function curry3<S0, S1, S2, T>(
+    f: (arg0: S0, arg1: S1, arg2: S2) => T
+): (arg0: S0) => (arg1: S1) => (arg2: S2) => T {
     return (arg0: S0) => curry2((arg1: S1, arg2: S2) => f(arg0, arg1, arg2));
 }
-
